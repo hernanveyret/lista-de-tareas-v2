7cf5ac7a347968fdb73a427dd453719f28a0ed13
hernanveyret <hernanveyret@hotmail.com>
2025-01-18 09:25:36 -0300

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins:[
+    react(),
+    VitePWA({
+      registerType:'autoUpdate',
+      manifest:{
+        name:'Lista de tareas',
+        short_name:'LTA',
+        description:'Sistema para generar listas de tareas',
+        theme_color:'#FFFFFF',
+        background_color:'#FFFFFF',
+        display:'standalone',
+        icons:[
+          {
+            src:'/icons/icon-192x192.png',
+            sizes:'192x192',
+            type:'image/png'
+          },
+          {
+            src:'/icons/icon-512x512.png',
+            sizes:'512x512',
+            type:'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
+
+
